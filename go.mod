ac32c32472e1bdc316f706bb46d27c1e16463587
hui <learner.hui@gmail.com>
2021-01-29 15:24:40 +0800

    ngrok watch

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module file-watch
 go 1.14
 
 require (
+	github.com/pkg/errors v0.8.1
 	github.com/prometheus/common v0.4.0
 	github.com/spf13/viper v1.7.1 // indirect
 	github.com/urfave/cli/v2 v2.3.0 // indirect
--- watch/ngrok.go
+++ watch/ngrok.go
@@ -3,6 +3,7 @@ package watch
 import (
     "bytes"
     "fmt"
+    "github.com/pkg/errors"
     "net/http"
     "os/exec"
     "time"
@@ -25,11 +26,12 @@ func (n *Ngrok) Change(c chan string) {
     
     for {
         if n.ping() {
-            if s := n.status(); s != "1" {
-                c <- s
+            if s, _ := n.status(); s != "0"{
+                c <- "ngrok 当前状态:" + s + " 跳过"
                 return
             }
-    
+            // 通知
+            c <- "准备启动 ngrok"
             if err := exec.Command("bash", "-c", "systemctl start ngrok").Run(); err != nil {
                 c <- fmt.Sprintf("启动 ngrok 失败, 错误信息: %v, 正在尝试重启...", err)
                 if err := exec.Command("bash", "-c", "systemctl restart ngrok").Run(); err != nil {
@@ -37,7 +39,7 @@ func (n *Ngrok) Change(c chan string) {
                     close(c)
                 }
             }
-            
+            c <- "重启 ngrok 完成"
             if newChannel := n.newAddress(); newChannel != "" {
                 c <- n.newAddress()
             }
@@ -59,15 +61,15 @@ func (n *Ngrok) ping() bool {
     return false
 }
 
-func (n *Ngrok) status() string {
+func (n *Ngrok) status() (string, error) {
     var out bytes.Buffer
     cmd := exec.Command("/bin/bash", "-c", "systemctl status ngrok | grep running | wc -l")
     cmd.Stdout = &out
     if err := cmd.Run(); err != nil {
-        return fmt.Sprintf("获取 ngrok 运行状态失败, 错误信息: %v", err)
+        return "0", errors.Errorf("获取 ngrok 运行状态失败, 错误信息: %v", err)
     }
     
-    return out.String()
+    return out.String(), nil
 }
 
 func (n *Ngrok) newAddress() string {
